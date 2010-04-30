b37f65e4a8fb8bfca7b05f29bfb9227c27c3cfca
Mikael Bung <phyrex1an@hotmail.com>
2010-04-30 11:54:54 +0200

    Seperated the input field and tabs from the main chat ui

--- thirc.js
+++ thirc.js
@@ -1,15 +1,13 @@
-window.addEvent('domready', function(){
+$(document).ready(function(){
     var root = $('#ircchat');
     var handler = new IRCHandler();
     var irc = new IRCClient(handler);
-    new IRCPingClient(handler);
-    var irc_client = null; //new APE.IrcClient({'root':root,'irc':irc}); // this should eventually be moved to THIrcUI
-//    handler.registerEventHandler(irc_client); 
+    new IRCPingClient(handler); 
     var client = new APE.Client();
     var ui = new THIrcUI(handler, 'irctester', root);
 
     var startIrc = function() {        
-        ui.start(new client.core.TCPSocket(), irc_client, irc);
+        ui.start(new client.core.TCPSocket(), irc);
     };
 
     var startCore = function() {
@@ -20,8 +18,8 @@ window.addEvent('domready', function(){
     var onDisconnect = function() {
         root.html('Connection failed...');
     };
-
-    root.html('Connecting to irc server...');
+    
+    ui.connecting();
     client.addEvent('load', startCore);
     client.addEvent('apeDisconnect', onDisconnect);
 
