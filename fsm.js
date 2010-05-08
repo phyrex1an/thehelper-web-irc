1f7fa18b059ebf8fb21d4372e7f641fe1c013fbf
Mikael Bung <phyrex1an@hotmail.com>
2010-05-08 22:23:13 +0200

    Added the fsm file

---
+++ fsm.js
@@ -0,0 +1,20 @@
+var FSM = function(input, states, startstate, state) {
+    this.states = states;
+    this.currentState = this.states[startstate];
+    this.state = state;
+    var self = this;
+    for(var i in input) {
+        this[input[i]] = (function(eventName) {
+            return (function(d) {
+                var nextStateName = self.currentState(eventName, d, self.state);
+                if (nextStateName == null) {
+                    for(var i in input) {
+                        self[input] = undefined;
+                    };
+                } else {
+                    self.currentState = self.states[nextStateName];
+                }
+            });
+        })(input[i]);
+    };
+};
